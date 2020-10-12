74f3c1528706be8fd0c5e13b14c13de3a4255908
mitrofin <mitrofn@gmail.com>
2020-10-12 16:33:35 +0300

    done

--- gallery-creation.js
+++ gallery-creation.js
@@ -15,5 +15,36 @@ const createImagesGallary = ({ preview, original, description }) => {
   </li>`;
 };
 const makeItemsGallery = images.map(createImagesGallary).join('');
-
 containerImages.insertAdjacentHTML('beforeend', makeItemsGallery);
+
+const lightBox = document.querySelector(".lightbox");
+const openModal = document.querySelector('.lightbox__image');
+const closeModal = document.querySelector('[data-action="close-lightbox"]');
+
+containerImages.addEventListener('click', onClick);
+
+function onClick(evt) { 
+   evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  lightBox.classList.add("is-open");
+  openModal.src = evt.target.dataset.source;
+  openModal.alt = evt.target.alt;
+
+};
+
+closeModal.addEventListener('click', onCloseModal);
+function onCloseModal(evt) {
+  if (evt.target.nodeName === 'IMG') {
+    return;
+  }
+  lightBox.classList.remove('is-open');
+  openModal.removeAttribute('src');
+  openModal.removeAttribute("alt");
+};
+
+
+
+  
+
